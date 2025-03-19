5d65de2995a3c08c40a66252a86d955cfb52844b
BoaN235 <noahgoata@outlook.com>
2025-03-19 19:49:22 -0400

    Add ability progress bars and input configuration for abilities

--- char/char_0/char_0_controll.gd
+++ char/char_0/char_0_controll.gd
@@ -1,26 +1,36 @@
 extends CharacterBody2D
 
-
-
 @export_category("Stats")
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 @export var max_double_jumps = 2
 
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var jumps = 0
 
+var progressbars = []
+
 func _ready():
-	pass
+	progressbars = [
+		get_node("Camera2D/Control/GridContainer/Ability_1/ProgressBar"),
+		get_node("Camera2D/Control/GridContainer/Ability_2/ProgressBar"),
+		get_node("Camera2D/Control/GridContainer/Ability_3/ProgressBar"),
+		get_node("Camera2D/Control/GridContainer/Ability_4/ProgressBar"),
+		get_node("Camera2D/Control/GridContainer/Ability_5/ProgressBar"),
+		get_node("Camera2D/Control/GridContainer/Ability_6/ProgressBar"),
+		get_node("Camera2D/Control/GridContainer/Ability_7/ProgressBar"),
+		get_node("Camera2D/Control/GridContainer/Ability_8/ProgressBar"),
+	]
+
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	elif  is_on_floor():
+	elif is_on_floor():
 		jumps = 0
+	$Camera2D/Control/GridContainer/Ability_1/ProgressBar.value += 1
 
 	# Handle jump.
 	if Input.is_action_just_pressed("con_jump"):
@@ -37,3 +47,9 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func _ability_bar_value_changed(value):
+	print(value)
+	if value == 100:
+		for bar in progressbars:
+			bar.visible = false
