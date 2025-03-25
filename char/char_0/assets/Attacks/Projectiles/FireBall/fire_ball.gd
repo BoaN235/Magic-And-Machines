e549d677079ca67fc5ac2dba6b3def447fdc7bcc
BoaN235 <noahgoata@outlook.com>
2025-03-24 21:39:03 -0400

    Implement fireball ability with progress bars and movement logic

--- char/char_0/assets/Attacks/Projectiles/FireBall/fire_ball.gd
+++ char/char_0/assets/Attacks/Projectiles/FireBall/fire_ball.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
-
+@onready var sprite = $AnimatedSprite2D
+var movment = 5
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position += Vector2(1,0)
+	if movment >= 1:
+		position += Vector2(movment, 0)
+	else:
+		position += Vector2(movment, 0)
+
+func direction(facing):
+	movment = facing * 5
+	if movment <= -1:
+		sprite.flip_h = false
--- char/char_0/char_0_controll.gd
+++ char/char_0/char_0_controll.gd
@@ -5,9 +5,12 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 @export var max_double_jumps = 2
 
+
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var jumps = 0
+var facing = 1
 
 var progressbars = []
 var cooldowns = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
@@ -38,17 +41,21 @@ func _physics_process(delta):
 			velocity.y = JUMP_VELOCITY
 			jumps += 1
 
+	if Input.is_action_just_pressed("ability_1"):
+		if progressbars[0].visible == false:
+			var fireball = preload("res://char/char_0/assets/Attacks/Projectiles/FireBall/fire_ball.tscn")
+			var fireballins = fireball.instantiate()
+			fireballins.position = self.position
+			fireballins.direction.call(facing)
+			self.get_parent().add_child(fireballins)
+
 	# Handle abilities
 	for i in range(8):
 		if Input.is_action_just_pressed("ability_%d" % (i + 1)) and cooldowns[i] <= 0:
 			cooldowns[i] = cooldown_times[i]
 			progressbars[i].visible = true
 
-	if Input.is_action_just_pressed("ability_1"):
-		var fireball = preload("res://char/char_0/assets/Attacks/Projectiles/FireBall/fire_ball.tscn")
-		var fireballins = fireball.instantiate()
 
-	# Update cooldowns and progress bars
 	for i in range(8):
 		if cooldowns[i] > 0:
 			cooldowns[i] -= delta
@@ -56,14 +63,14 @@ func _physics_process(delta):
 		else:
 			progressbars[i].visible = false
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+
 	var direction = Input.get_axis("con_left", "con_right")
+	facing = direction
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
 
 func _ability_bar_value_changed(value):
